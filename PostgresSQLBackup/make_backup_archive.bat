305e912fd26cde5c74a0353e1493faca08601aa7
Bushuev Roman <bushuev.roman@gmail.com>
2020-10-11 23:25:35 +0300

    dump and sql. Restore dump.

---
+++ PostgresSQLBackup/make_backup_archive.bat
@@ -0,0 +1,15 @@
+SET PGPATH=C:\"Program Files"\PostgreSQL\12\bin\
+SET SVPATH=C:\Yandex\YandexDisk\backups\
+SET DBUSR=karma_admin
+SET DBDUMP=.dump
+SET DBDUMPSQL=.sql
+SET PGPASSWORD=karma_admin
+SET GITPATH=C:\Users\homan\Documents\repo\Murr\SqlScripts\MainDatabaseBackup\
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+SET BACKUPFILE=karma%mydate%_%date:~-4,4%_%date:~-10,2%_%date:~-7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+set BACKUPFILE=%BACKUPFILE: =0%
+%PGPATH%pg_dump.exe -h localhost -p 5432 -U %DBUSR% -Fc -d karma >%SVPATH%%BACKUPFILE%%DBDUMP%
+%PGPATH%pg_dump.exe -h localhost -p 5432 -U %DBUSR% -d karma >%SVPATH%%BACKUPFILE%%DBDUMPSQL%
+%PGPATH%pg_dump.exe -h localhost -p 5432 -U %DBUSR% -Fc -d karma >%GITPATH%%BACKUPFILE%%DBDUMP%
+%PGPATH%pg_dump.exe -h localhost -p 5432 -U %DBUSR% -d karma >%GITPATH%%BACKUPFILE%%DBDUMPSQL%
---
+++ PostgresSQLBackup/restore_backup_archive_for_local.bat
@@ -0,0 +1,9 @@
+SET PGPATH=C:\"Program Files"\PostgreSQL\12\bin\
+SET SVPATH=C:\Users\homan\Documents\repo\Murr\SqlScripts\MainDatabaseBackup\
+SET DBUSR=postgres
+SET PGPASSWORD=roman
+
+SET BACKUPFILE=karma_2020_11_10_19_51_06.dump
+%PGPATH%dropdb.exe -h localhost -p 5432 -U %DBUSR% karma_test
+%PGPATH%createdb.exe -h localhost -p 5432 -U %DBUSR% -T template0 karma_test
+%PGPATH%pg_restore.exe -h localhost -p 5432 -U %DBUSR% -d karma_test %SVPATH%%BACKUPFILE%
